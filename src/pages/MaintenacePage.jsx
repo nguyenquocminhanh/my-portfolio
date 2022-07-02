4ec58feb0955bbeaacfaf3883cce258275491379
minh <minhhanh.nguyen001@umb.edu>
2022-07-02 16:52:16 -0400

    Add maintenace mode

--- src/pages/MaintenacePage.jsx
+++ src/pages/MaintenacePage.jsx
@@ -13,7 +13,7 @@ class MaintenacePage extends Component {
                     <div class="container min-vh-100 d-flex flex-column justify-content-between text-center py-7 py-md-8">
                         <a className="navbar-brand text-white" href="#home">MINH NGUYEN PORTFOLIO</a>
                         <div class="my-5 text-white">
-                            <h1>The new website is under construction.</h1>
+                            <h1>The website is under maintenance.</h1>
                             <p class="lead mb-7">I`ll be here soon with my new awesome site.</p>
                             <div class="row">
                                 <div class="col-md-9 col-lg-7 col-xl-6 mx-auto">
