49a7cbe27ad34f9cf8723ec5a84d3196efa4f328
minh <minhhanh.nguyen001@umb.edu>
2022-07-03 12:24:23 -0400

    Update certification

--- src/pages/MaintenacePage.jsx
+++ src/pages/MaintenacePage.jsx
@@ -11,7 +11,7 @@ class MaintenacePage extends Component {
                         <div className="overlay-inner bg-gray-900 opacity-50"></div>
                     </div>
                     <div class="container min-vh-100 d-flex flex-column justify-content-between text-center py-7 py-md-8">
-                        <a className="navbar-brand text-white" href="#home">MINH NGUYEN PORTFOLIO</a>
+                        <a className="navbar-brand text-white" href="#home">MINH NGUYEN</a>
                         <div class="my-5 text-white">
                             <h1>The website is under maintenance.</h1>
                             <p class="lead mb-7">I`ll be here soon with my new awesome site.</p>
