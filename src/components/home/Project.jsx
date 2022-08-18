d41679a8ac1933d691a3a31483edd1fc35742a4f
minh <minhhanh.nguyen001@umb.edu>
2022-08-18 00:27:54 -0400

    update number of preview projects

--- src/components/home/Project.jsx
+++ src/components/home/Project.jsx
@@ -14,7 +14,7 @@ class Portfolio extends Component {
         <div class="album py-3 bg-light">
           <div class="container" style={{maxWidth: '1320px'}}>
             <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-              {this.props.projects.map(project => {
+              {this.props.projects.slice(0, 3).map(project => {
                 return <ProjectPreview key={project['id']} project={project}/>
               })}
             </div>
